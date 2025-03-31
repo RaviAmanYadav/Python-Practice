d411f63bb888af43a14a274b6688c48265f027b9
RaviAmanYadav <amanbittuyadav@gmail.com>
2025-03-31 22:28:15 +0530

    Youtube manager completed

--- Youtube Manager App/youtube_manager.py
+++ Youtube Manager App/youtube_manager.py
@@ -1,4 +1,3 @@
-
 import json
 
 
@@ -8,33 +7,49 @@ def load_data():
             return json.load(file)
     except FileNotFoundError:
         return []
-    
+
 
 def save_data_helper(videos):
-    with open('youtube.txt', "w") as file:
+    with open("youtube.txt", "w") as file:
         json.dump(videos, file)
-   
+
 
 def list_all_videos(videos):
     for index, video in enumerate(videos, start=1):
-        print(f"{index}. {video['name']}")
+        print(f"{index}. name : {video['name']} duration : {video["time"]}")
 
 
 def add_video(videos):
     name = input("Enter video name =>")
     time = input("Enter video time =>")
-    videos.append({'name' : name, "time" : time})
+    videos.append({"name": name, "time": time})
     save_data_helper(videos)
-
+    print("Your new video has been added")
 
 
 def update_video(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the video number to update => "))
+    if 1 <= index <= len(videos):
+        name = input("Enter new video name => ")
+        time = input("Enter new video time => ")
+        videos[index - 1] = {"name": name, "time": time}
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected")
+
+    print("You list has been updated")
 
 
 def delete_video(videos):
-    pass
-
+    list_all_videos(videos)
+    index = int(input("Enter the video number to be deleted =>"))
+    if 1 <= index <= len(videos):
+        del videos[index - 1]
+        save_data_helper(videos)
+    else:
+        print("Invalid index number has been selected.")
+    print("Your selected choice has been deleted")
 
 
 def main():
