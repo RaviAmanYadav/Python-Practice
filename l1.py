6b83ecb888ada9b0a244ba505234a932660aef18
RaviAmanYadav <amanbittuyadav@gmail.com>
2026-03-07 22:19:16 +0530

    L1

--- l1.py
+++ l1.py
@@ -5,18 +5,27 @@ def add_student():
     student_name = input("Enter the name of the student=>")
     return student_name
 
+
 def remove_student(name):
     stu.remove(name)
-    
+
 
 stu = []
 while True:
-    print("1. Add student")
+    print("1. Add student.")
+    print("2. Remove student.")
     print("3. Exit")
+    print("4. Show student list.")
     choice = int(input("Enter your choice => "))
     match choice:
         case 1:
             stu.append(add_student())
+        case 2:
+            name = input("Enter the name of the student => ")
+            if name in stu:
+                remove_student(name)
+            else:
+                print("Student name is not present.")
         case 3:
             exit()
         case 4:
