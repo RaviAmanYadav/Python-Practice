7daebff090e3869c6a2b6368173ed99c621631e3
RaviAmanYadav <amanbittuyadav@gmail.com>
2026-03-08 19:20:02 +0530

    Updation of a student is half completed

---
+++ student_management_system/l2.py
@@ -0,0 +1,70 @@
+def add_student(roll_number):
+    student_name = input("Enter the name of the student => ")
+    student_age = input("Enter student age => ")
+    student_father_name = input("Enter the name of student's father => ")
+    student_mother_name = input("Enter the name of student's mother => ")
+
+    student_details = {
+        "roll": roll_number,
+        "name": student_name,
+        "age": student_age,
+        "father": student_father_name,
+        "mother": student_mother_name,
+    }
+
+    return student_details
+
+
+def remove_student(roll_number):
+    if roll_number in stu:
+        del stu[roll_number]
+        print("Student record has been removed.")
+    else:
+        print("Roll number doesn't exit! Please enter valid roll number.")
+
+
+def update_student_details():
+
+    while True:
+        roll_number = int(input("Enter the roll number for updatation => "))
+        if roll_number in stu:
+            print("Enter new details.")
+            name = input("Enter name of the student => ")
+            stu[roll_number]["name"] = name
+            print("Student details has been updated.")
+            break
+        else:
+            print("Roll number not found! Please enter correct roll number")
+
+
+stu = {}
+roll_numeber = 1
+
+while True:
+    print("1. Add studnet.")
+    print("2. Remove student.")
+    print("3. Show all student details.")
+    print("4. Update student's details.")
+    print("5. Exit.")
+
+    choice = int(input("Choose the option =>"))
+
+    match choice:
+        case 1:
+            student = add_student(roll_numeber)
+            stu[roll_numeber] = student
+            roll_numeber += 1
+        case 2:
+            roll_number = int(input("Enter the roll number to be removed => "))
+            remove_student(roll_number)
+        case 3:
+            for roll_number, details in stu.items():
+                print("\nRoll number : ", roll_number)
+                for key, value in details.items():
+                    print(f"{key} : {value}")
+        case 4:
+            update_student_details()
+        case 5:
+            exit()
+        case _:
+            print("Please choose the correct option.")
