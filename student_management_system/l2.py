ec9c6d4d6fcceea97130dd64d61926c4e90a46d8
RaviAmanYadav <amanbittuyadav@gmail.com>
2026-03-11 20:07:33 +0530

    finally class concepts are over

--- student_management_system/l2.py
+++ student_management_system/l2.py
@@ -1,6 +1,5 @@
-
 class Parent:
-    def __init__(self, father_name,mother_name):
+    def __init__(self, father_name, mother_name):
         self.father_name = father_name
         self.mother_name = mother_name
 
@@ -11,7 +10,6 @@ class Student:
         self.name = name
         self.age = age
         self.parent = parent
-        
 
 
 class School:
@@ -19,73 +17,72 @@ class School:
         self.students = {}
         self.roll_number = 1
 
-
-    def add_student(roll_number):
-        student_name = input("Enter the name of the student => ")
-        student_age = input("Enter student age => ")
+    # Adding the students.
+    def add_student(self):
+        name = input("Enter the name of the student => ")
+        age = input("Enter student age => ")
         student_father_name = input("Enter the name of student's father => ")
         student_mother_name = input("Enter the name of student's mother => ")
+        parent = Parent(student_father_name, student_mother_name)
+        student = Student(self.roll_number, name, age, parent)
+        self.students[self.roll_number] = student
+        print("Student add successfully")
+        self.roll_number += 1
 
-        student_details = {
-            "roll": roll_number,
-            "name": student_name,
-            "age": student_age,
-            "father": student_father_name,
-            "mother": student_mother_name,
-        }
-
-        return student_details
-
-    def remove_student(roll_number):
-        if roll_number in stu:
-            del stu[roll_number]
+    # deletion of a students
+    def remove_student(self):
+        roll_number = int(input("Enter the roll number to be removed => "))
+        if roll_number in self.students:
+            del self.students[roll_number]
             print("Student record has been removed.")
         else:
             print("Roll number doesn't exit! Please enter valid roll number.")
 
-
-    def update_student_details():
+    # Updation of students
+    def update_student_details(self):
 
         while True:
             roll_number = int(input("Enter the roll number for updatation => "))
-            if roll_number in stu:
+            if roll_number in self.students:
                 print("Enter new details.")
                 name = input("Enter name of the student => ")
-                stu[roll_number]["name"] = name
+                self.students[roll_number].name = name
                 print("Student details has been updated.")
                 break
             else:
                 print("Roll number not found! Please enter correct roll number")
 
-    while True:
-        print("1. Add studnet.")
-        print("2. Remove student.")
-        print("3. Show all student details.")
-        print("4. Update student's details.")
-        print("5. Exit.")
+    def show_student(self):
+        if not self.students:
+            print("No students are present.")
+            return
 
-        choice = int(input("Choose the option =>"))
-
-        match choice:
-            case 1:
-                student = add_student(roll_numeber)
-                stu[roll_numeber] = student
-                roll_numeber += 1
-            case 2:
-                roll_number = int(input("Enter the roll number to be removed => "))
-                remove_student(roll_number)
-            case 3:
-                for roll_number, details in stu.items():
-                    print("\nRoll number : ", roll_number)
-                    for key, value in details.items():
-                        print(f"{key} : {value}")
-            case 4:
-                update_student_details()
-            case 5:
-                exit()
-            case _:
-                print("Please choose the correct option.")
+        for roll_number, students in self.students.items():
+            print("Roll number => ", roll_number)
+            print("Name : ", students.name)
 
 
+school = School()
 
+while True:
+    print("1. Add studnet.")
+    print("2. Remove student.")
+    print("3. Show all student details.")
+    print("4. Update student's details.")
+    print("5. Exit.")
 
+    choice = int(input("Choose the option =>"))
+
+    match choice:
+        case 1:
+            school.add_student()
+        case 2:
+            school.remove_student()
+        case 3:
+            school.show_student()
+        case 4:
+            school.update_student_details()
+        case 5:
+            exit()
+        case _:
+            print("Please choose the correct option.")
